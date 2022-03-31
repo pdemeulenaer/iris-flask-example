f25b96f2cdf1ef74e768e3a158b194dbf18d2c1c
pdemeulenaer <pdemeulenaer@gmail.com>
2022-03-31 13:37:38 +0000

    refactor request.py

--- request.py
+++ request.py
@@ -2,13 +2,11 @@ import requests
 import json
 
 # 1. For and INDIVIDUAL prediction (using /predict_api)
-# Using CURL: curl -F "file=@iris_to_predict.csv" http://127.0.0.1:5000/api_predict_file
+# Using CURL: TODO
 # Using python request:
 
 url = 'http://localhost:5000/api_predict'
-
 data = requests.post(url,json={'sepal length (cm)': 7, 'sepal width (cm)': 3, 'petal length (cm)': 4, 'petal width (cm)': 1.5})
-
 print(data.json())
 
 
