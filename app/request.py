74fe7749cbceb95c78187fd66053c74e76c23b83
pdemeulenaer <pdemeulenaer@gmail.com>
2022-04-04 12:01:43 +0000

    unlock request for both api_predict and api_predict_file

--- app/request.py
+++ app/request.py
@@ -4,9 +4,9 @@ import json
 # 1. For and INDIVIDUAL prediction (using /predict_api)
 # Using CURL: TODO
 # Using python request:
-# url = 'http://localhost:5000/api_predict'
-# data = requests.post(url,json={'sepal length (cm)': 7, 'sepal width (cm)': 3, 'petal length (cm)': 4, 'petal width (cm)': 1.5})
-# print(data.json())
+url = 'http://localhost:5000/api_predict'
+data = requests.post(url,json={'sepal length (cm)': 7, 'sepal width (cm)': 3, 'petal length (cm)': 4, 'petal width (cm)': 1.5})
+print(data.json())
 
 
 # 2. For a CSV file prediction (using /predict_api_file)
