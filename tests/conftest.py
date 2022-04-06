641082939119763f80aabc91b6f6db683e51f520
pdemeulenaer <pdemeulenaer@gmail.com>
2022-04-06 11:21:22 +0000

    fix the conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,7 @@ from app import app
 
 @pytest.fixture(scope="session")
 def client():
+    '''
+    This fixture creates the client Flask app that will be used in all Flask tests
+    '''
     return app.test_client()
