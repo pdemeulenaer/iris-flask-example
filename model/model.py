58e038abbdbf3947fae4b8860b5756b4e4863357
pdemeulenaer <pdemeulenaer@gmail.com>
2022-04-06 11:18:38 +0000

    simplifying model code

--- model/model.py
+++ model/model.py
@@ -5,37 +5,33 @@ Purpose is to produce the pickle artefact that can be used in the Flask app
 
 import pickle
 import numpy as np
-# import pandas as pd
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score, roc_auc_score
 
+# Data loading
+iris_dataset = load_iris()
+x = iris_dataset["data"]
+y = iris_dataset["target"]
 
-#classifier code
+# Train, Test split
+x_train, x_test, y_train, y_test = train_test_split(x,y,random_state=0, test_size=0.3)
 
-#loading the data
-iris_dataset=load_iris()
+# Model declaration and fitting
+clf = KNeighborsClassifier(n_neighbors=2)
+clf.fit(x_train, y_train)
 
-#Splitting the data into features and label
-x=iris_dataset["data"]
-y=iris_dataset["target"]
+# Performance metric
+y_pred = clf.predict(x_test)
+acc = accuracy_score(y_test, y_pred)
+print ('acc:',acc)
 
-#Splitting data into tarining and test data
-x_train, x_test, y_train, y_test=train_test_split(x,y,random_state=0, test_size=0.3)
+# Saving the model artifact locally
+pickle.dump(clf, open("model.pkl", "wb"))
 
-kclassifier=KNeighborsClassifier(n_neighbors=2)
-
-#fitting training  data into KNN classifier
-kclassifier.fit(x_train, y_train)
-
-
-#Dumping the model into pickle
-pickle.dump(kclassifier, open("model.pkl", "wb"))
-
-model=pickle.load(open("model.pkl", "rb"))
-
-xn=np.array([[7,3,4,1.5]])
-
-xpred=kclassifier.predict(xn)
-
-print(iris_dataset["target_names"][xpred])
+# Model loading for simple testing on dummy data
+model = pickle.load(open("model.pkl", "rb"))
+data = np.array([[2,8,1,3]])
+predictions = model.predict(data)
+print(iris_dataset["target_names"][predictions])
